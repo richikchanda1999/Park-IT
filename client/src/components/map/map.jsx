38e729cc2ffdd5d83a7f4d087d77ae61497753d0
Richik Chanda <richikchanda1999@gmail.com>
2021-03-25 14:38:06 +0530

    Search bar for client rendering done

--- client/src/components/map/map.jsx
+++ client/src/components/map/map.jsx
@@ -1,16 +1,21 @@
-import React, { useState, useEffect, useCallback, memo } from 'react';
-import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api';
+import React, {useState, useEffect, useCallback, memo} from 'react';
+import {GoogleMap, useJsApiLoader, Marker, InfoWindow} from '@react-google-maps/api';
 import mapStyles from "./mapStyles";
-import { BookButton } from "./common";
+import {BookButton} from "./common";
 import {navigate} from "hookrouter";
 import PlacesAutocomplete from 'react-places-autocomplete';
 import {
     geocodeByAddress,
     geocodeByPlaceId,
     getLatLng,
-  } from 'react-places-autocomplete';
-import Autocomplete from 'react-google-autocomplete';  
-const { REACT_APP_GOOGLE_MAP_KEY_PAID, REACT_APP_API_BACKEND } = process.env;
+} from 'react-places-autocomplete';
+import "./autocompleteStyle.css";
+import { classnames } from './helpers';
+import Autocomplete from 'react-google-autocomplete';
+import FloatingComponent from "react-floating-component";
+
+const {REACT_APP_GOOGLE_MAP_KEY_PAID, REACT_APP_API_BACKEND} = process.env;
+const libraries = ["places"];
 
 function MyMap(props) {
     const [selectedPark, setSelectedPark] = useState(null);
@@ -19,7 +24,7 @@ function MyMap(props) {
     const [parkData, setParkData] = useState([]);
     const [map, setMap] = useState(null);
     const [center, setMapCenter] = useState({lat: 22.572645, lng: 88.363892});
-    const [address,setAddress]= useState("");
+    const [address, setAddress] = useState("");
     let lotsFetched = false;
     let statusFetched = false;
     let markersMap = new Map();
@@ -31,7 +36,6 @@ function MyMap(props) {
 
     const onLoad = useCallback(function callback(map) {
         setMap(map);
-        autoComplete();
         console.log(map.zoom);
     }, [])
 
@@ -45,8 +49,8 @@ function MyMap(props) {
 
             let requestOption = {
                 method: "POST",
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ 'latitude': latitude, 'longitude': longitude }),
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({'latitude': latitude, 'longitude': longitude}),
             };
             console.log(requestOption);
             let res = await fetch(`${REACT_APP_API_BACKEND}/map/nearby_coordinates`, requestOption);
@@ -70,8 +74,8 @@ function MyMap(props) {
         if (!statusFetched) {
             let requestOption = {
                 method: "POST",
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ 'place_id': place_id }),
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({'place_id': place_id}),
             };
             console.log(requestOption);
             let res = await fetch(`${REACT_APP_API_BACKEND}/map/get_live_status`, requestOption);
@@ -92,7 +96,7 @@ function MyMap(props) {
         await getParkingLots(center.lat(), center.lng());
     }
 
-    async function booking_nav(){
+    async function booking_nav() {
         console.log(selectedPark);
         navigate('/booking', false, {selectedPark: selectedPark});
     }
@@ -102,108 +106,124 @@ function MyMap(props) {
         const latLng = await getLatLng(results[0]);
         setAddress(value);
         setMapCenter(latLng);
-      }
-   
-    const { isLoaded } = useJsApiLoader({
+    }
+
+    const {isLoaded} = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: REACT_APP_GOOGLE_MAP_KEY_PAID,
-        libraries: 'places'
+        libraries: libraries
     })
 
-    function autoComplete()
-    {
+    function autoComplete() {
+        console.log("HERE!");
         return (
-              <PlacesAutocomplete
-                value={address}
-                onChange={setAddress}
-                onSelect={handleSelect}
-              >
-                {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-                  <div>
-                    <input {...getInputProps({ placeholder: "Type address" })} />
-                    <div>
-                      {loading ? <div>...loading</div> : null}
-                      {suggestions.map(suggestion => {
-                        const style = {
-                          backgroundColor: suggestion.active ? "#41b6e6" : "#fff"
-                        };
+                <PlacesAutocomplete
+                    onChange={setAddress}
+                    value={address}
+                    onSelect={handleSelect}
+                    shouldFetchSuggestions={address.length > 2}
+                >
+                    {({ getInputProps, suggestions, getSuggestionItemProps }) => {
                         return (
-                          <div {...getSuggestionItemProps(suggestion, { style })}>
-                            {suggestion.description}
-                          </div>
-                        );
-                      })}
-                    </div>
-                  </div>
-                )}
-              </PlacesAutocomplete>
-          );
+                            <div className="Demo__search-bar-container">
+                                <div className="Demo__search-input-container">
+                                    <input
+                                        {...getInputProps({
+                                            placeholder: 'Type Address...',
+                                            className: 'Demo__search-input',
+                                        })}
+                                    />
+                                </div>
+                                {suggestions.length > 0 && (
+                                    <div className="Demo__autocomplete-container">
+                                        {suggestions.map(suggestion => {
+                                            const className = classnames('Demo__suggestion-item', {
+                                                'Demo__suggestion-item--active': suggestion.active,
+                                            });
 
-        // var url=`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=1600+Amphitheatre&key=${REACT_APP_GOOGLE_MAP_KEY_PAID}&sessiontoken=1234567890`;
-        //
-        // let requestOption = {
-        //     method: "GET",
-        //     headers: { 'Content-Type': 'application/json' ,"Access-Control-Allow-Origin": "*"},
-        // };
-        // console.log(requestOption);
-        // let res = await fetch(url, requestOption);
-        // // console.log(res);
-        // if (res.status === 200) {
-        //     let val = await res.json();
-        //     console.log(val);
-        // }
+                                            return (
+                                                /* eslint-disable react/jsx-key */
+                                                <div
+                                                    {...getSuggestionItemProps(suggestion, { className })}
+                                                >
+                                                    <strong>
+                                                        {suggestion.formattedSuggestion.mainText}
+                                                    </strong>{' '}
+                                                    <small>
+                                                        {suggestion.formattedSuggestion.secondaryText}
+                                                    </small>
+                                                </div>
+                                            );
+                                            /* eslint-enable react/jsx-key */
+                                        })}
+                                        <div className="Demo__dropdown-footer">
+                                            <div>
+                                                <img
+                                                    src={require('./powered_by_google_default.png')}
+                                                    className="Demo__dropdown-footer-image"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
+                        );
+                    }}
+                </PlacesAutocomplete>
+        );
     }
 
     useEffect(() => {
         getParkingLots(22.572645, 88.363892);
     }, [getParkingLots]);
 
-    return isLoaded ? (autoComplete()
-        // <div>
-        //     {autoComplete()}
-        // <GoogleMap
-        //     mapContainerStyle={containerStyle}
-        //     center={center}
-        //     zoom={14}
-        //     onLoad={onLoad}
-        //     onUnmount={onUnmount}
-        //     options={{styles: mapStyles}}
-        //     onDragEnd={handleDrag}
-        // >
-        //     {parkData.map(park => (
-        //         <Marker
-        //             key={park['latitude'] + " " + park['longitude']}
-        //             position={{
-        //                 lat: park['latitude'],
-        //                 lng: park['longitude']
-        //             }}
-        //             onClick={async () => {
-        //                 let ret = await getStatus(park['place_id']);
-        //                 setSelectedPark(park);
-        //                 setSelectedParkCAP(ret['CAP']);
-        //                 setSelectedParkTPS(ret['TPS']);
-        //             }}
-        //         />
-        //     ))}
-        //     {selectedPark && (
-        //         <InfoWindow
-        //             position={{
-        //                 lat: selectedPark['latitude'],
-        //                 lng: selectedPark['longitude']
-        //             }}
-        //             onCloseClick={() => {
-        //                 setSelectedPark(null);
-        //             }}
-        //         >
-        //             <div>
-        //                 <h4>{selectedPark['name']}</h4>
-        //                 <p>Current status: {selectedParkCAP} / {selectedParkTPS}</p>
-        //                 <BookButton type="submit" onClick={booking_nav}>BOOK</BookButton>
-        //             </div>
-        //         </InfoWindow>
-        //     )}
-        // </GoogleMap>
-        // </div>
+    // return isLoaded ? autoComplete() : <></>
+    return isLoaded ? (
+        <div>
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={14}
+                onLoad={onLoad}
+                onUnmount={onUnmount}
+                options={{styles: mapStyles}}
+                onDragEnd={handleDrag}
+            >
+                {parkData.map(park => (
+                    <Marker
+                        key={park['latitude'] + " " + park['longitude']}
+                        position={{
+                            lat: park['latitude'],
+                            lng: park['longitude']
+                        }}
+                        onClick={async () => {
+                            let ret = await getStatus(park['place_id']);
+                            setSelectedPark(park);
+                            setSelectedParkCAP(ret['CAP']);
+                            setSelectedParkTPS(ret['TPS']);
+                        }}
+                    />
+                ))}
+                {selectedPark && (
+                    <InfoWindow
+                        position={{
+                            lat: selectedPark['latitude'],
+                            lng: selectedPark['longitude']
+                        }}
+                        onCloseClick={() => {
+                            setSelectedPark(null);
+                        }}
+                    >
+                        <div>
+                            <h4>{selectedPark['name']}</h4>
+                            <p>Current status: {selectedParkCAP} / {selectedParkTPS}</p>
+                            <BookButton type="submit" onClick={booking_nav}>BOOK</BookButton>
+                        </div>
+                    </InfoWindow>
+                )}
+                <FloatingComponent>{autoComplete()}</FloatingComponent>
+            </GoogleMap>
+        </div>
     ) : <></>
 }
 
