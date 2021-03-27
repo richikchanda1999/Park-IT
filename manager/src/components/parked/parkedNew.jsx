26d8c468b16ad9820f4fd41ec5166955f9af802f
Roshan Kumar <rk722579@gmail.com>
2021-03-27 22:24:55 +0530

    Manager Table Modified

--- manager/src/components/parked/parkedNew.jsx
+++ manager/src/components/parked/parkedNew.jsx
@@ -1,49 +1,49 @@
 import React, {useState} from 'react';
+import MaterialTable from 'material-table';
 
 const {REACT_APP_API_BACKEND} = process.env;
 
 
 function ManagerHistory(){
-    const email = "ashok@gmail.com";
-    const [userStatus, setUserStatus] = useState([]);
-    let parkingData = [];
-    var statusFetched = false;
-    const [startFetched, setstartFetched] = useState(false);
-
-    async function getHistory(email) {
-        if (!statusFetched) {
-            statusFetched = true;
-            let requestOption = {
-                method: "POST",
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ 'email': email}),
-            };
-            console.log(requestOption);
-            var res = await fetch(`${REACT_APP_API_BACKEND}/manager/parking/get_current_parking`, requestOption);
-            console.log(res);
-            var getn = await res.json();
-            console.log(getn);
-            for( let lot in getn)
-                parkingData.push(getn[lot]);
-            console.log(parkingData);
-            setUserStatus(parkingData);
-            console.log(userStatus);
-            if (res.status === 200) {
-                console.log("Failed");
-            }  
-        }
-    }
-
-    if(!startFetched){
-        setstartFetched(true);
-        getHistory(email);
-        
-    }
     
+    const column = [
+        { title: 'Vehicle No.', field: 'vehicle',
+          cellStyle: {
+            backgroundColor: '#039be5',
+            color: '#FFF'
+          },
+        },
+        { title: 'Parking Lot', field: 'parking_lot' },
+        { title: 'Entry Time', field: 'entry_time'},
+        { title: 'Status', field: 'status' }
+    ]
 
+    const data = [
+        { vehicle: 'PY01 8065', parking_lot: 'East Coast', entry_time: '03/26/2021 15:00' , status: 'Parked' },
+        { vehicle: 'DL04 3154 ', parking_lot: 'East Coast', entry_time: '03/26/2021 12:30' , status: 'Booked' },
+        { vehicle: 'GC43 1643', parking_lot: 'East Coast', entry_time: '03/26/2021 13:20' , status: 'Parked' },
+    ]
     return(
         <div>
-            Hi
+            <MaterialTable title = "Current Parking Status"
+            data = {data}
+            columns={column}
+            options={{
+                pageSize: 10,
+                exportButton: true,
+                sorting: true,
+                headerStyle: {
+                  zIndex: 2,
+                  backgroundColor: '#01579b',
+                  color: '#FFF'
+                },
+                rowStyle: {
+                    "&:hover":{
+                        backgroundColor: '#01579b',
+                    }
+                }
+              }}
+            />
         </div>
     );
 
